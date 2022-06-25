53fc50852fad04b77e8073858210d399c250d73d
Atanas Solakov <atanassolakov@gmail.com>
2022-06-25 22:09:36 +0300

    Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from './PartsForm/Home';
 import {ThemeProvider} from "@mui/material";
 import { theme } from "./Theme/Theme";
+import SimpleAccordion from './DisplayParts/SimpleAccordion'
 
 function App() {
     return (
         <ThemeProvider theme={theme}>
             <BrowserRouter>
                 <Routes>
+                    <Route path="/accordion" element={<SimpleAccordion/>}/>
                     <Route path="/home" element={<Home/>}/>
                 </Routes>
             </BrowserRouter>
