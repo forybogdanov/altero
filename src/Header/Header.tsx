dc5dcbbfeb8c917c1ab110617950ef34d1e8b0b8
forybogdanov <nikiforn04@gmail.com>
2022-06-26 11:14:00 +0300

    add logo and change brake parameters

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,12 +1,14 @@
 import { Grid, Typography } from "@mui/material";
 import logo from "../logo.svg";
+import altero from "../altero-logo.svg";
 import { grey } from "../Theme/Theme";
 
 export const Header = () => {
     return(
         <Grid container bgcolor={grey} marginBottom="20px" alignItems="center">
-            <img src={logo} width="240px" height="52px" style={{margin: "10px"}}/>
-            <Typography color="white" fontSize={32}>- Altero</Typography>
+            <img src={altero} width="150px" height="44px" style={{margin: "10px"}}/>
+            <Typography marginTop={"20px"} marginLeft={"10px"} marginRight={"10px"} color="white" fontSize={18}>by</Typography>
+            <img src={logo} width="168px" height="36px" style={{margin: "10px"}}/>
         </Grid>
     );
 };
