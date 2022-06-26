3090f284e96495bbe43531d837a01cb62309af89
Atanas Solakov <atanassolakov@gmail.com>
2022-06-26 12:04:17 +0300

    not Final

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { SpecificationsForm } from './PartsForm/SpecificationsForm';
 import {ThemeProvider} from "@mui/material";
 import { theme } from "./Theme/Theme";
 import SimpleAccordion from './DisplayParts/SimpleAccordion'
-
+import Recommendation from "./DisplayParts/Recommendation";
 function App() {
     return (
         <ThemeProvider theme={theme}>
@@ -12,6 +12,7 @@ function App() {
                 <Routes>
                     <Route path="/accordion" element={<SimpleAccordion/>}/>
                     <Route path="/" element={<SpecificationsForm/>}/>
+                    <Route path="/recommendation" element={<Recommendation/>}/>
                 </Routes>
             </BrowserRouter>
         </ThemeProvider>
